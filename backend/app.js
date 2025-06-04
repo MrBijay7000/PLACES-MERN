82119db06fbfd07b3fa45533df90ffe8fd3b6f23
BIJAY SHRESTHA <shresthabzay95@gmail.com>
2025-06-04 22:35:23 +0545

    app.js updated

--- backend/app.js
+++ backend/app.js
@@ -30,17 +30,8 @@ app.use(
 
 app.use(bodyParser.json());
 
+app.options("*", cors());
 app.use("/uploads/images", express.static(path.join("uploads", "images")));
-
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
-  next();
-});
 
 app.use("/api/places", placesRoutes);
 app.use("/api/users", usersRoutes);
